30ba2056a3fb4539ab86330d736e50ad03efb7bd
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-05-24 12:56:30 -0700

    Update Comic_Info.go
    Added function explanation comment and future plan comments

--- Comic_Info.go
+++ Comic_Info.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+  // Prints comic book info based on certain parameters
   var publisher, writer, artist, title string
   var year, pageNumber int
   var grade float32
@@ -29,3 +30,9 @@ func main() {
 
   fmt.Println(title, "written by", writer, "drawn by", artist, "published in", year, "by", publisher, "with", pageNumber, "pages, with a condition grade of", grade)
 }
+
+/*definition
+My goals for the project are to:
+1. Add the variables into the function
+2. Setting the variables to be user input
+*/
